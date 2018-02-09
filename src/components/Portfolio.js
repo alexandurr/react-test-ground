43d95d1342701043dfbe82506acd71334f0007ed
Anthony Wiloughby <awilloughby@bestunion.com>
2018-02-09 12:19:59 -0500

    creating the components to display

---
+++ src/components/Portfolio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Portfolio = () => (
+    <div>
+        This is a list of my work
+        <Link>Item 1</Link>
+        <Link>Item 2</Link>
+    </div>
+)
