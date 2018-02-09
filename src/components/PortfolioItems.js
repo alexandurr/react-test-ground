84e9f6b075c7b84770bda9b89eae72ff2ef803d1
Anthony Wiloughby <awilloughby@bestunion.com>
2018-02-09 13:00:31 -0500

    updating porfolio items

--- src/components/PortfolioItems.js
+++ src/components/PortfolioItems.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PortfolioItems = (props) => {
     return (
         <div>
-            <h2>Item {props.match.params.id}</h2>
+            <h2>A thing I have done</h2>
             <p>This is item {props.match.params.id}</p>
         </div>
     );
